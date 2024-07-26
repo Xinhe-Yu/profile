277844755706b129680a1170e6b20c5e3d0564c2
Xinhe YU <aspasia.yu@gmail.com>
2024-07-26 18:42:25 +0200

    add light toggle

---
+++ minesweeper/lib/lightToggle.js
@@ -0,0 +1,13 @@
+console.log("toggle load")
+const checkbox = document.getElementById('toggle');
+const body = document.querySelector('body');
+
+checkbox.addEventListener('change', () => {
+  if (checkbox.checked) {
+    body.classList.add("light");
+    body.classList.remove("dark")
+  } else {
+    body.classList.add("dark");
+    body.classList.remove("light");
+  }
+});
