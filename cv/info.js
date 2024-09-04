ef359716781d8da16bc80383e1f44719c97d18b6
Xinhe YU <aspasia.yu@gmail.com>
2024-09-04 20:52:37 +0200

    flashcards

---
+++ cv/info.js
@@ -0,0 +1,135 @@
+const contact = [
+  {
+    icon: "",
+    content: "+33-6-70-27-69-35",
+    url: "",
+  },
+  {
+    icon: "",
+    content: "Paris, France",
+    url: "",
+  },
+  {
+    icon: "",
+    content: "xinhe.yu.dsa@gmail.com",
+    url: "mailto:xinhe.yu.dsa@gmail.com",
+  },
+  {
+    icon: "",
+    content: "xinhe.de",
+    url: "http://xinhe.de",
+  },
+  {
+    icon: "",
+    content: "Xinhe-YU",
+    url: "https://github.com/Xinhe-Yu",
+  }
+
+]
+
+const education = [
+  {
+    school: "Le Wagon",
+    degree: "Coding Bootcamp (RNCP level 6)",
+    start_year: 2024,
+    end_year: null,
+    description: "Immersive courses in full-stack development, <span class='secondary'>Rank 1</span> bootcamp in the world.",
+    location: "Paris, France",
+  },
+  {
+    school: "École pratique des Hautes Études",
+    degree: "PhD in Art History",
+    start_year: 2019,
+    end_year: 2024,
+    description: "Specialized in iconology of the Athenian vases, supervised by François Queyrel. Graduation on <strong>April 27, 2024</strong>.",
+    location: "Paris, France",
+  },
+  {
+    school: "École Normale Supérieure de Paris",
+    degree: "Master's Degree in Humanities",
+    start_year: 2017,
+    end_year: 2019,
+    description: "A leading-edge training in high-level research in the field of the classical studies.",
+    location: "Paris, France",
+  },
+  {
+    school: "Le Wagon",
+    degree: "Bachelor's Degree in History",
+    start_year: 2013,
+    end_year: 2017,
+    description: "QS World University Rankings 2017/18:  <span class='secondary'>Rank 40</span>.",
+    location: "Shanghai, China",
+  },
+]
+
+const training = [
+  {
+    year: 2023,
+    institut: "Meta",
+    course: "Front-End Developer",
+    url: "https://www.coursera.org/account/accomplishments/professional-cert/RM6H7MYPMG82",
+    description: "Coursera online training in <italic>HTML/CSS/JavaScript, React, Jest, Figma</italic>.",
+  },
+  {
+    year: 2021,
+    institut: "Google",
+    course: "IT Automation with Python",
+    url: "https://www.coursera.org/account/accomplishments/professional-cert/CX9RKB3MKAKS",
+    description: "Coursera online training in <italic>Python (debugging and testing), Git, Script shell, Saas</italic>.",
+  },
+  {
+    year: 2020,
+    institut: "University of Michigan",
+    course: "Python for Everybody",
+    url: "https://www.coursera.org/account/accomplishments/specialization/AWJK4MY6L8EW",
+    description: "Coursera online training in <italic>Python (OOP, scalping), SQL</italic>.",
+  },
+  {
+    year: 2020,
+    institut: "University of Vienna",
+    course: "German language summer school in Vienna, Austria.",
+    url: null,
+    description: null,
+  },
+]
+
+const experience = [
+  {
+    title: "Xinhe's Personal Site",
+    post: "Web Development",
+    url: "https://xinhe.de",
+    description: "This is my personal site ! Designed with care using React for a dynamic and interactive user interface. Additionally, this site also benefits from Chakra UI, which simplifies the implementation of a responsive and accessible design.",
+    start_year: "2023",
+    end_year: null,
+    location: null,
+    source_code: "https://github.com/Xinhe-Yu/xinhe-yu.github.io",
+  },
+  {
+    title: "ALCN Database",
+    post: "Web Development",
+    url: "https://xinhe-yu.github.io/ALCN/",
+    description: "The website is a dedicated database for the translation of ancient names. Built primarily with Bootstrap and JavaScript, data management is handled by JSON, with Python scripts used for processing and maintaining the data.",
+    start_year: "",
+    end_year: null,
+    location: null,
+    source_code: "https://github.com/Xinhe-Yu/ALCN",
+  },
+  {
+    title: "Château of Montsoreau - Museum of Contemporary Art",
+    post: "Cultural Mediator",
+    url: null,
+    description: "Welcome and guide visitors on tours; orginize events including concerts, theaters, and festivals at the castle.",
+    start_year: 2022,
+    end_year: null,
+    location: null,
+  },
+  {
+    title: "Scientific Translation from French to Chinese",
+    post: "Translator",
+    url: null,
+    description: "Publications in the well-known presses (3 articles in Fudan University Press and 1 forthcoming book in the Commercial Press).",
+    start_year: 2018,
+    end_year: "Present",
+    location: null,
+  },
+]
